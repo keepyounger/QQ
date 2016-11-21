9d71f2c517d39545e93d3ce5d17349abf3d53f67
lixy <ky@keepyounger.com>
2016-11-21 17:35:12 +0800

    update

--- QQRedBag/QQRedBag/NSObject+QQMessageRecallModule.h
+++ QQRedBag/QQRedBag/NSObject+QQMessageRecallModule.h
@@ -7,7 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <vector>
 
-@interface NSObject (QQMessageRecallModule)
+@interface NSObject (QQRecall)
 
 @end
+
+typedef union {
+    unsigned long long _field1;
+    unsigned long long _field2;
+    unsigned long long _field3;
+} XXUnion_znrfyA;
+
+typedef struct RecallItem {
+    bool _field1;
+    unsigned long long _field2;
+    XXUnion_znrfyA _field3;
+    unsigned _field4;
+    unsigned _field5;
+    unsigned _field6;
+    unsigned _field7;
+    unsigned _field8;
+    unsigned _field9;
+} RecallItem;
+
+typedef struct RecallModel {
+    /*function-pointer*/ void** _field1;
+    int _field2;
+    bool _field3;
+    std::vector<RecallItem *> _field4;
+} RecallModel;
